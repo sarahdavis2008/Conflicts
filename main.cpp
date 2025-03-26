cadd10f874619b8281bf9ae6fccb5d50b0dface6
sarahdavis2008 <sdavis2@nic.edu>
2025-03-26 20:42:35 +0000

    added add function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 using namespace std;
 
+int add(int a, int b);
+
 int main()
 {
     cout << "Hello World\n";
     return 0;
+}
+
+int add(int a, int b)
+{
+    return a + b;
 }
